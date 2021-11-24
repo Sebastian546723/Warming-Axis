3f73ffd0e51e095794122008eb6cfeafdc34f35c
daniel <daniel.zeballos@usm.cl>
2021-11-23 21:26:19 -0300

    Co-authored-by: Carlos Espinoza Rodriguez (Alumno) <carlos.espinozar@usm.cl> Co-authored-by: Francisco Dominguez Montecinos (Alumno) <francisco.dominguezm@usm.cl> Co-authored-by: sebastianarrieta123@gmail.com <sebastianarrieta123@gmail.com>

--- noticias/models.py
+++ noticias/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    Title = models.CharField(max_length=100, blank=True)
+    Description = models.CharField(max_length=100, blank=True)
+    ImgNews = models.CharField(max_length=100, blank=True)
+    URL = models.CharField(max_length=100, blank=True)
+    Source = models.CharField(max_length=100, blank=True)
--- noticias/views.py
+++ noticias/views.py
@@ -1,11 +1,41 @@
 from django.shortcuts import render
 from newsapi import NewsApiClient
+from noticias import models as ModelObject
+import time
+import threading
 
 newsapi = NewsApiClient(api_key='3e10e6f5c0d046a7aef93f1db3e778a9')
 
-noticiasAPI = newsapi.get_everything(q='Climate Change',
+def SearchLoop():
+    print("hello")
+    def searchNews():
+        newsLoaded = 0
+        while newsLoaded < 3:
+            PageNews = 1
+            top_headlines = newsapi.get_everything(q='Cambio climatico',
                                     sort_by='relevancy',
                                     language='es')
+            for a in top_headlines["articles"]:
+                model = ModelObject.News.objects.filter(Title = a["title"])
+                if len(model) == 0:
+                    if newsLoaded < 3:
+                        ModelObject.News.objects.create(
+                            Title = a["title"],
+                            Description = a["description"],
+                            ImgNews = a["urlToImage"],
+                            URL = a["url"],
+                            Source = a["source"]["name"]
+                        )
+                        newsLoaded += 1
+            
+            PageNews +=1
+
+    while True:
+        searchNews()
+        time.sleep(5)
+
+hilo = threading.Thread(target=SearchLoop)
+hilo.start()
 
 def noticias(request):
-    return render(request, "noticias.html", noticiasAPI)
+    return render(request, "noticias.html")
