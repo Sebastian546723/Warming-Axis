87d8741e7f6d2a70c5757b5616bc77bec2a76261
Sebastian Arrieta <sebastianarrieta123@gmail.com>
2021-12-01 18:05:10 -0300

    commit

--- noticias/views.py
+++ noticias/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render
 from newsapi import NewsApiClient
 from noticias import models as ModelObject
+import schedule
 import time
 import threading
 
 newsapi = NewsApiClient(api_key='3e10e6f5c0d046a7aef93f1db3e778a9')
 
-def SearchLoop():
-    def searchNews():
+NewsList = {'articles':[]}
+ForoId = {"ids": []}
+
+def getIDs():
+        ForoId["ids"] = []
+        IDs= list(ModelObject.News.objects.values('id'))
+
+        for i in range(1,4):
+            getNews = ModelObject.News.objects.get(id = IDs[-i]['id'])
+
+            ForoId["ids"].append(IDs[-i]['id'])
+            NewsList['articles'].append(
+                {"title": getNews.Title, 
+                "description": getNews.Description, 
+                "urlToImage": getNews.ImgNews, 
+                "url": getNews.URL, 
+                "name": getNews.Source}
+                )
+
+def searchNews():
+        NewsList["articles"] = []
         newsLoaded = 0
         PageNews = 1
         while newsLoaded < 3:
-            top_headlines = newsapi.get_everything(q='Cambio climatico',
+            top_headlines = newsapi.get_everything(q='Climate Change',
                                     sort_by='relevancy',
                                     language='es')
             for a in top_headlines["articles"]:
@@ -29,31 +49,23 @@ def SearchLoop():
             
             PageNews +=1
 
+if len(ModelObject.News.objects.values('id')) == 0:
+    searchNews()
+
+getIDs()
+
+def SearchLoop():
+    
+    schedule.every().day.at("18:05").do(searchNews)
+    schedule.every().day.at("18:07").do(getIDs)
+
     while True:
-        searchNews()
-        time.sleep(86400)
+        schedule.run_pending()
+        time.sleep(1)
 
 hilo = threading.Thread(target=SearchLoop)
 hilo.start()
 
-NewsList = {'articles':[]}
-ForoId = {"ids": []}
-
-IDs= list(ModelObject.News.objects.values('id'))
-
-for i in range(1,4):
-    getNews = ModelObject.News.objects.get(id = IDs[-i]['id'])
-
-    ForoId["ids"].append(IDs[-i]['id'])
-    NewsList['articles'].append(
-        {"title": getNews.Title, 
-        "description": getNews.Description, 
-        "urlToImage": getNews.ImgNews, 
-        "url": getNews.URL, 
-        "name": getNews.Source}
-        )
-
-print(ForoId)
 def noticias(request):
     return render(request, "noticias.html", NewsList)
 
